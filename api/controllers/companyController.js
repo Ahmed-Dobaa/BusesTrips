1c7cb58bf39fb7b2b192db86b3333e2e39cd4a9f
Ahmed-Dobaa <engahmeddobaa@gmail.com>
2022-05-14 18:03:10 +0200

    companies

--- api/controllers/companyController.js
+++ api/controllers/companyController.js
@@ -39,8 +39,8 @@ module.exports = {
     let language = request.headers.language;
     let companies = null;
      try {
-      companies = await models.sequelize.query(`SELECT c.id, c.name, c.email, c.phoneNumber, s.id userId,
-         (select lookupDetailName from lookup_details l where l.id = s.status) status
+      companies = await models.sequelize.query(`SELECT c.id, c.name, c.email, c.phoneNumber, s.id userId, s.status,
+         (select lookupDetailName from lookup_details l where l.id = s.status) statusName
          FROM companies c, users s
          where c.id = s.companyId
          and c.deletedAt is null `, { type: QueryTypes.SELECT });
