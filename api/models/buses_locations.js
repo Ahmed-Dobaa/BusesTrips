ef0f45ecdd6c6ac5a5dd82e652f1e652005e1bd2
Ahmed Mobasher <Ahmed.Mobasher@geidea.net>
2023-01-10 08:53:33 +0200

    change route name column in bus locations table type to sttring

--- api/models/buses_locations.js
+++ api/models/buses_locations.js
@@ -10,7 +10,7 @@ module.exports = function (sequelize, DataTypes) {
       allowNull: false,
     },
     routeName: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING(100),
       allowNull: false
     },
     companyId: {
