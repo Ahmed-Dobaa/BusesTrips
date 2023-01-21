3a4d4cf151500146a6de2e2738b0e4e567e19d71
Ahmed Mobasher <Ahmed.Mobasher@geidea.net>
2023-01-21 17:53:10 +0200

    route payment

--- api/controllers/points.js
+++ api/controllers/points.js
@@ -251,7 +251,7 @@ module.exports = {
           and b.deletedAt is null
       `, { type: QueryTypes.SELECT });
 
-         let routePayment = await models.sequelize.query(`SELECT * from routes_payment WHERE routeId = ${points[i].routeId} and b.deletedAt is null`, { type: QueryTypes.SELECT });
+         let routePayment = await models.sequelize.query(`SELECT * from routes_payment WHERE routeId = ${points[i].routeId} and deletedAt is null`, { type: QueryTypes.SELECT });
 
            points[i]["routePoints"]= route;
            points[i]["routePayment"]= routePayment[0];
