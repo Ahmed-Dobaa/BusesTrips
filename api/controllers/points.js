9c5e053569e136d47ae862b3d4a8c183d7e7a96e
Ahmed Mobasher <Ahmed.Mobasher@geidea.net>
2023-04-22 17:55:11 +0200

    search trips

--- api/controllers/points.js
+++ api/controllers/points.js
@@ -327,8 +327,8 @@ module.exports = {
     try {
 
       let tripDays = await models.sequelize.query(`
-          SELECT SELECT id, day, tripId, from, to, (select name from trips t where t.id = td.tripId) tripName,
-          (SELECT routeName from buses_locations where id = (SELECT busRouteId from trips t where t.id = td.tripId) ) routName, (SELECT id from buses_locations where id = (SELECT busRouteId from trips t where t.id = td.tripId) ) routeId from allenap_bus.trips_days
+          SELECT id, day, tripId, 'from', 'to', (select name from trips t where t.id = td.tripId) tripName,
+          (SELECT routeName from buses_locations where id = (SELECT busRouteId from trips t where t.id = td.tripId) ) routName, (SELECT id from buses_locations where id = (SELECT busRouteId from trips t where t.id = td.tripId) ) routeId from allenap_bus.trips_days td
           where day = '${request.payload.day}'
           and tripId in (SELECT id from allenap_bus.trips where busRouteId in 
                           (SELECT id from allenap_bus.buses_locations where startPoint =${request.payload.startPoint} and endPoint =${request.payload.endPoint} ) )
@@ -336,8 +336,8 @@ module.exports = {
 
       if (tripDays.length == 0) {
         tripDays = await models.sequelize.query(`
-        SELECT SELECT id, day, tripId, from, to, (select name from trips t where t.id = td.tripId) tripName,
-        (SELECT routeName from buses_locations where id = (SELECT busRouteId from trips t where t.id = td.tripId) ) routName, (SELECT id from buses_locations where id = (SELECT busRouteId from trips t where t.id = td.tripId) ) routeId from allenap_bus.trips_days
+        SELECT id, day, tripId, 'from', 'to', (select name from trips t where t.id = td.tripId) tripName,
+        (SELECT routeName from buses_locations where id = (SELECT busRouteId from trips t where t.id = td.tripId) ) routName, (SELECT id from buses_locations where id = (SELECT busRouteId from trips t where t.id = td.tripId) ) routeId from allenap_bus.trips_days td
         where day = '${request.payload.day}' 
         and tripId in (SELECT id from allenap_bus.trips where busRouteId in 
                         (
